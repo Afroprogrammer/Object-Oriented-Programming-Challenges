dd73245882a82359069cdcd98e5fd21cb1c3e764
afroprogrammer <forsonfranklin@gmail.com>
2020-06-25 23:01:56 +0000

    Define a Constructor Function

---
+++ define-a-constructor-function.js
@@ -0,0 +1,11 @@
+/*Constructors are defined with a capitalized name to distinguish them from other functions that are not constructors.
+Constructors use the keyword this to set properties of the object they will create. Inside the constructor, this refers to the new object it will create.
+Constructors define properties and behaviors instead of returning a value as other functions might.*(hint)*/
+
+//Create a constructor, Dog, with properties name, color, and numLegs that are set to a string, a string, and a number, respectively.
+
+function Dog(){
+  this.name = "tiny Dog";
+  this.color = "white";
+  this.numLegs = 2;
+}
